a083ae716e3748b33eb47dc3fe86ab4c00f916cf
El12A <153304482+El12A@users.noreply.github.com>
2024-03-11 21:15:35 +0000

    Update Shape.cs

--- Assets/RedoScripts/Projectile Scripts/Shape.cs
+++ Assets/RedoScripts/Projectile Scripts/Shape.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 namespace PhysicsProjectileSimulator
 {
+    //abstract class mainly for just containing references to abstract functions for children classes and different measurements and variables specific to shape of projectile
     public abstract class Shape : Projectile
     {
         public float volume;
         public float mass;
 
+        // all possible measurements for all different shapes avialable as options to user
         public float radius;
         public float length;
         public float width;
         public float height;
 
         public abstract float CalculateVolume();
+        // used for adjusting the scale of gameobject so in game mesh is the right size in metres
         public abstract void SetScale();
         public abstract float CalculateMass();
     }
 }
-
