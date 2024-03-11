9fd041de2def7f66e6e5fb5df392dc566d046319
El12A <153304482+El12A@users.noreply.github.com>
2024-03-11 21:24:14 +0000

    Update SphereProjectile.cs

--- Assets/RedoScripts/Projectile Scripts/Projectile Shapes/SphereProjectile.cs
+++ Assets/RedoScripts/Projectile Scripts/Projectile Shapes/SphereProjectile.cs
@@ -7,6 +7,7 @@ namespace PhysicsProjectileSimulator
 {
     public class SphereProjectile : Shape
     {
+        // this is set in the editor and is a mesh of a sphere 3d object
         [SerializeField] private Mesh sphereMesh;
 
         public override float CalculateVolume()
@@ -22,16 +23,19 @@ namespace PhysicsProjectileSimulator
             float newScale = 2.0f * radius;
             projectileObject.transform.localScale = new Vector3(newScale, newScale, newScale);
         }
+        // this is a simple equation of density * volume = mass 
         public override float CalculateMass()
         {
             mass = projectile.density * volume;
             return mass;
         }
+        // This sets the mesh component of the gameobject to be the sphereMesh specifically
         public void SetMesh()
         {
             MeshFilter currentMeshFilter = projectileObject.GetComponent<MeshFilter>();
             currentMeshFilter.mesh = sphereMesh;
         }
+        // updates the variables of the rigidbody component so that its mass and drag is that which the user has entered.
         public override void UpdateRigidbody()
         {
             projectileRb.mass = mass;
@@ -39,4 +43,3 @@ namespace PhysicsProjectileSimulator
         }
     }
 }
-
