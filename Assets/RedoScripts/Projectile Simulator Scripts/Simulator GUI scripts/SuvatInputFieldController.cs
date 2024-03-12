dbdb588c06720346a94c20352de09d51aca4750f
El12A <153304482+El12A@users.noreply.github.com>
2024-03-12 11:01:05 +0000

    Update SuvatInputFieldController.cs

--- Assets/RedoScripts/Projectile Simulator Scripts/Simulator GUI scripts/SuvatInputFieldController.cs
+++ Assets/RedoScripts/Projectile Simulator Scripts/Simulator GUI scripts/SuvatInputFieldController.cs
@@ -7,8 +7,10 @@ using UnityEngine.Windows;
 
 namespace PhysicsProjectileSimulator
 {
+    // this class handles the inputs for the suvat variables that have been chosen from the suvat variable dropdown
     public class SuvatInputFieldController : VariableController
     {
+        // references to the inputField prefabs and parents transform
         [SerializeField] private GameObject singleInputPrefab;
         [SerializeField] private GameObject vector3InputPrefab;
         [SerializeField] private Transform parentTransform;
@@ -16,11 +18,11 @@ namespace PhysicsProjectileSimulator
         private TMP_InputField[] inputFields;
         private Vector3 spawnPosition;
 
-        
-
         // Start is called before the first frame update
         protected void Start()
         {
+            // for all the input prefabs add their inputs to the list of input fields
+            // then add listeners to all the inputfields so that when their value is changedd the inputFieldRestrictor function called OnValueChanged will be called and the same for OnEndEdit()
             foreach (GameObject inputprefab in variableController.spawnInputsPrefabList)
             {
                 inputFields = inputprefab.GetComponentsInChildren<TMP_InputField>();
@@ -33,15 +35,9 @@ namespace PhysicsProjectileSimulator
             }
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
+        // calls the correctfunction and gets the right location to spawn the new InputField
         public void SetInputActive(int dropdownNum, string variable)
         {
-            Debug.Log(variable);
             // if dropdown being changed is the first one make the spawnposition for the input field be as described
             // spawn position differs based on which dropdown it is
             if (dropdownNum == 0)
@@ -73,6 +69,7 @@ namespace PhysicsProjectileSimulator
 
         }
 
+        // spawns the actual input field and adds listeners to it
         private void spawnNewInputField(GameObject inputPrefab, Vector3 spawnPos, int index)
         {
             // if the number of spawned gameobjects which are stored in spawnInputsPrefabList are 3 or more first delete the one which is to be replaced.
@@ -98,12 +95,14 @@ namespace PhysicsProjectileSimulator
             // add the gameobject to the list at the specified index.
             variableController.spawnInputsPrefabList.Insert(index, spawnedInputField);
         }
-        
+
+        // called when input field has a value changed in its input box
         public void OnAnyInputFieldValueChanged(TMP_InputField input)
         {
             inputFieldRestrictor.UpdateInputField(input);
             inputFieldRestrictor.OnValueChanged();
         }
+        // called when the input field is no longer being edited
         public void OnAnyInputFieldEndEdit(TMP_InputField input)
         {
             inputFieldRestrictor.UpdateInputField(input);
@@ -111,4 +110,3 @@ namespace PhysicsProjectileSimulator
         }
     }
 }
-
