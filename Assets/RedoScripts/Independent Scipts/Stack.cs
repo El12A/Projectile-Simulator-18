cac9a6e14a4c743a606ebcbd6f985d2d0b61559a
El12A <153304482+El12A@users.noreply.github.com>
2024-03-12 11:12:49 +0000

    Update Stack.cs

--- Assets/RedoScripts/Independent Scipts/Stack.cs
+++ Assets/RedoScripts/Independent Scipts/Stack.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,7 +12,7 @@ namespace PhysicsProjectileSimulator
         private int count;
         private int capacity;
 
-        //constructor for Stack
+        //constructor for the Stack
         public Stack(int capacity)
         {
             // make the capacity whatever the user enters but obviosuly with a minimum of 1
@@ -21,11 +20,15 @@ namespace PhysicsProjectileSimulator
             stackArray = new T[this.capacity];
             topOfStack = -1;
             count = 0;
+
         }
+        // returns the count of the stack (how many elements are in the stack)
         public int GetCount()
         {
             return count;
         }
+
+        // this method is for looking at the top of the stack and returning error exception if stack is empty
         public T Peek()
         {
             // check if stack is empty if so throw empty stack error
@@ -36,17 +39,23 @@ namespace PhysicsProjectileSimulator
             // else if stack is not empty return the top item
             return stackArray[topOfStack];
         }
+
+        // this function is for adding items to the top of the stack it will also resize it if currently full
+        // the resize is especially handy for the timestamp stack implementation as it is likely the user will have the projectile in motion for longer then the initial length call
         public void Push(T item)
         {
             // if stack is full then resize the array making it twice as big.
             if (count == capacity)
             {
-                Array.Resize(ref stackArray, count * 2); 
+                Array.Resize(ref stackArray, count * 2);
             }
             topOfStack++;
             count++;
             stackArray[topOfStack] = item;
         }
+
+        // this method will remove and return the top element of the stack
+        // it will throw error if the stack is empty though
         public T Pop()
         {
             // check if stack is empty if so throw empty stack error
@@ -60,14 +69,12 @@ namespace PhysicsProjectileSimulator
             count--;
             return item;
         }
-        //clear all elements in stack and reset count and topOfStack pointer
+        //clears all  the elements inside of the stack and resets the count and the topOfStack pointer
         public void Clear()
         {
-            topOfStack = -1; 
-            count = 0; 
-            Array.Clear(stackArray, 0, stackArray.Length); 
+            topOfStack = -1;
+            count = 0;
+            Array.Clear(stackArray, 0, stackArray.Length);
         }
-
     }
 }
-
