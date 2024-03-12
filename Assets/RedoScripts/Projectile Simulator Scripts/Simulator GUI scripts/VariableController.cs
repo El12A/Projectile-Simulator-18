e5959e2aa8db186c41f48c87b07c6fa58affcff4
El12A <153304482+El12A@users.noreply.github.com>
2024-03-12 10:30:12 +0000

    Update VariableController.cs

--- Assets/RedoScripts/Projectile Simulator Scripts/Simulator GUI scripts/VariableController.cs
+++ Assets/RedoScripts/Projectile Simulator Scripts/Simulator GUI scripts/VariableController.cs
@@ -9,17 +9,23 @@ namespace PhysicsProjectileSimulator
 {
     public class VariableController : PhysicsSimulator
     {
+
+        // reference to error message text, trajectory and its line renderer
         public TMP_Text ErrorMessageText;
         [SerializeField] private GameObject trajectoryObject;
         private LineRenderer trajectoryLineRenderer;
+        // variables for the draw trajectory recursive function
         private int maxIterations = 1000;
         private float maxTime = 100f;
         private float timeStep = 0.1f;
+        // boolean for the trajectory whether it should be active or not active
         private bool trajectory0n = false;
+        // references to the reveal variable buttons 
         [SerializeField] private GameObject HideVariableButton1;
         [SerializeField] private GameObject HideVariableButton2;
         [SerializeField] private Toggle airResistanceToggle;
 
+        // references to the different children scripts that need to be refernced in different children scripts hence the reason there is a reference here in the parent class
         public SuvatDropdownController SuvatDropdownController;
         public SuvatInputFieldController SuvatInputFieldController;
         public MissingSuvatTextController missingSuvatTextController;
@@ -31,23 +37,24 @@ namespace PhysicsProjectileSimulator
         // conatins at start: Accleration, Time
         public List<string> unselectedVariables;
 
+        // the important 5 suvat variables 3 of which are entered by the user and 2 of which are then calculated based off of the users inputs
         public Vector3 displacement;
         public Vector3 initialVelocity;
         public Vector3 finalVelocity;
         public Vector3 acceleration;
         public Vector3 time;
 
+        [SerializeField] private GameObject projectileShooter;
+
         private bool acceptInputs = true;
         private void Start()
         {
+            // get reference to the projectile script from the projectile gameobject
             projectile = GameObject.FindWithTag("projectile").GetComponent<Projectile>();
         }
-        // Update is called once per frame
-        void Update()
-        {
 
-        }
-
+        // based on which variables have been selected set the value of the suvat variable to that inputted the user
+        // this function is called when calculate Button is clicked
         public void UpdateInputtedVariables()
         {
             foreach (string var in selectedVariables)
@@ -77,6 +84,8 @@ namespace PhysicsProjectileSimulator
 
             }
 
+            // then we check if the values given are actuall in the allowed range
+            // if not an error message will be shown and variables wont be accepted as suvat inputs
             foreach (string var in selectedVariables)
             {
                 if (var == "Displacement")
@@ -126,47 +135,17 @@ namespace PhysicsProjectileSimulator
                 }
 
             }
-
+            
+            // if inputted variables have passed the test then say succesful input to the user and continue by calling update all text function of child class, hiding the text of the new calculated variables (calculated in child class), updating projectile shooter angle, and drawing the trajectory if trajectory toggle is set to true
             if (acceptInputs == true)
             {
-                foreach (string var in selectedVariables)
-                {
-                    int index = selectedVariables.IndexOf(var);
-                    if (var == "Displacement")
-                    {
-                        displacement = SetVariableVector3(index);
-                        Debug.Log("s");
-                    }
-
-                    if (var == "Initial Velocity")
-                    {
-                        initialVelocity = SetVariableVector3(index);
-                        Debug.Log("u");
-                    }
-                    if (var == "Final Velocity")
-                    {
-                        finalVelocity = SetVariableVector3(index);
-                        Debug.Log("v");
-                    }
-                    if (var == "Acceleration")
-                    {
-                        acceleration = SetVariableSingle(index);
-                        acceleration.x = 0.0f;
-                        acceleration.z = 0.0f;
-                        Debug.Log("a");
-
-                    }
-                    if (var == "Time")
-                    {
-                        time = SetVariableSingle(index);
-                        Debug.Log("t");
-                    }
-                }
                 ErrorMessageText.text = "Successfull Input";
                 missingSuvatTextController.UpdateAllText();
                 ResetHideButtons();
+                AdjustProjectileShooterAngle();
                 DrawTrajectory(projectile.projectileObject.transform.position, initialVelocity, 0);
             }
+            // else reset accepet inputs to true ready for another check
             else
             {
                 acceptInputs = true;
@@ -176,6 +155,8 @@ namespace PhysicsProjectileSimulator
 
         }
 
+        // this method will get the correct input prefab and get a vector 3 value from what is found in the input field
+        // this is called when trying to get what is in a vector 3 input field prefab eg what is entered for initial velocity, final velocity or displacement 
         private Vector3 SetVariableVector3(int index)
         {
             GameObject parentObject = spawnInputsPrefabList[index];
@@ -187,11 +168,11 @@ namespace PhysicsProjectileSimulator
             float inputText2 = float.Parse(inputFields[1].text);
             float inputText3 = float.Parse(inputFields[2].text);
 
-
-
             return new Vector3(inputText1, inputText2, inputText3);
         }
 
+        // this method returns a vector 3 with all the components equal to the float entered by the user in the single inputField
+        // used for time and acceleration, for acceleration however after being calle the x and z components are set to 0 as acceleration input is only for Y component
         private Vector3 SetVariableSingle(int index)
         {
             GameObject parentObject = spawnInputsPrefabList[index];
@@ -199,9 +180,9 @@ namespace PhysicsProjectileSimulator
             TMP_InputField[] inputFields = parentObject.GetComponentsInChildren<TMP_InputField>();
             float inputText = float.Parse(inputFields[0].text);
             return new Vector3(inputText, inputText, inputText);
-
         }
 
+        // this method will make sure that the variable is in the correct desired range return a bool for yes or no - true or false
         private bool CheckVariableInRange(float varComponent, float lowRange, float highRange)
         {
             if (varComponent >= lowRange && varComponent <= highRange)
@@ -211,14 +192,17 @@ namespace PhysicsProjectileSimulator
             return false;
         }
 
+        // resets the Reveal variable buttons called when valid input is given 
         private void ResetHideButtons()
         {
             HideVariableButton1.SetActive(true);
             HideVariableButton2.SetActive(true);
         }
 
+        // activates or deactivates the trajectory
         public void ActivateTrajectory()
         {
+            // if the trajectory is not yet on then activate it
             if (trajectory0n == false)
             {
                 trajectory0n = true;
@@ -226,48 +210,47 @@ namespace PhysicsProjectileSimulator
                 trajectoryLineRenderer = trajectoryObject.GetComponent<LineRenderer>();
                 DrawTrajectory(projectile.initialPosition, initialVelocity, 0);
             }
+            // else deactivate the trajectory
             else
             {
                 trajectory0n = false;
                 trajectoryObject.SetActive(false);
             }
-            
+
         }
-        //recursive algorithm to calculate trajectory path
+        //recursive algorithm that calculates and draws the trajectory path
         private void DrawTrajectory(Vector3 position, Vector3 velocity, int iteration)
         {
+            // if trajectory toggle is set to true then draw trajectory else do not
             if (trajectory0n == true)
             {
-
+                // stops the trajectory from having too many points so that the trajectory line renderer is never too performance heavy from rendering to many points
                 if (iteration >= maxIterations)
                 {
-                    // stops the trajectory from having too many points so that the trajectory line renderer is never too performance heavy from rendering to many points
                     Debug.LogWarning("Max iterations reached. Abort trajectory calculation");
                     return;
                 }
+                // This will stop the recursive algorithm when projectile trajectory reaches the ground as then it will no longer be visible so no point drawing any more points
                 if (position.y < 0)
                 {
-                    return; // This will stop the recursive algorithm when projectile trajectory reaches the ground
+                    return; 
                 }
+                // if points are being draw for a long time stop the trajetory calculation as becoming too performance heavy
                 if (iteration >= maxTime / timeStep)
                 {
                     Debug.LogWarning("Max time reached. Aborting trajectory calculation.");
                     return;
                 }
+                // add one to the position cound and set new position for the line renderer
                 trajectoryLineRenderer.positionCount = iteration + 1;
                 trajectoryLineRenderer.SetPosition(iteration, position);
-
+                // calculate nex position and velocity to apply for the next iteration of the recursive trajectory drawing function
                 Vector3 nextPosition = position + velocity * timeStep;
                 Vector3 nextVelocity = velocity + acceleration * timeStep;
 
+                // call the function again with the new values for position and velocity
                 DrawTrajectory(nextPosition, nextVelocity, iteration + 1);
             }
-        }
-
-        private Vector3 CalculatePositionAtTime(float time)
-        {
-            // Calculates the position of the projectile at a specific moment in time
-            return projectile.initialPosition + initialVelocity * time + 0.5f * acceleration * time * time;
         }
 
         // if toggle is on make the projectile drag equal whatever specified by projectile's drag variable else make it zero so no air resistance is experienced by the projectile
@@ -280,6 +263,11 @@ namespace PhysicsProjectileSimulator
             }
             projectile.projectileRb.drag = dragmultiplier * projectile.drag;
         }
-    }
 
+        // adjusts the angle rotation of the projectile shooter to that of the projectile at initial velocity
+        public void AdjustProjectileShooterAngle()
+        {
+            projectileShooter.transform.rotation = Vector3.Angle(initialVelocity);
+        }
+    }
 }
