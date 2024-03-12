adad84accfdd5398c46cf774060d82d76e35d68a
El12A <153304482+El12A@users.noreply.github.com>
2024-03-12 10:42:28 +0000

    Update MoveTarget.cs

--- Assets/RedoScripts/Projectile Simulator Scripts/Simulator GUI scripts/MoveTarget.cs
+++ Assets/RedoScripts/Projectile Simulator Scripts/Simulator GUI scripts/MoveTarget.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 
 namespace PhysicsProjectileSimulator
 {
+    // this class translates the target object based off of the displcement suvat variable.
     public class MoveTarget : VariableController
     {
         private void Start()
         {
+            // get reference to the projectile script from projectile gameobject
             projectile = GameObject.FindWithTag("projectile").GetComponent<Projectile>();
         }
         public void OnDisplacementChange()
         {
+            // if there has been a successful input then adjust the position of the target x distance from the projectile initial position, where x is the displacement suvat variable
             if (ErrorMessageText.text == "Successfull Input")
             {
                 transform.position = projectile.initialPosition + variableController.displacement;
@@ -20,4 +23,3 @@ namespace PhysicsProjectileSimulator
         }
     }
 }
-
